d60505d0ec8a973cc109b1a2e7ce8053b738537a
Christian Ampe <christianampe.work@gmail.com>
2020-10-27 22:22:23 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SearchPicker
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SearchPickerTests
+
+var tests = [XCTestCaseEntry]()
+tests += SearchPickerTests.allTests()
+XCTMain(tests)
